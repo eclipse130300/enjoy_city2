7f8588f8877e11e152704f5f98907c264382d37a
eclipse130300 <30523791+eclipse130300@users.noreply.github.com>
2020-05-04 19:11:41 +0300

    UI_remastered

---
+++ Assets/ThirdParty/DimitryTest/Scripts/Tabs/Tab.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Tab : MonoBehaviour , IPointerClickHandler
+{
+    [SerializeField] private Image inactivebackgroundIMG;
+    [SerializeField] private Image activebackgroundIMG;
+    [SerializeField] private Image activeIconIMG;
+    [SerializeField] private Image inactiveIconIMG;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        ActivateTab();
+    }
+
+    public void ActivateTab()
+    {
+
+    }
+
+    public void DeactivateTab()
+    {
+
+    }
+
+
+}
---
+++ Assets/ThirdParty/DimitryTest/Scripts/Tabs/TabGroup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<Tab> tabs;
+
+   public void Subscribe(Tab tabButton)
+    {
+        if(tabs == null)
+        {
+            tabs = new List<Tab>();
+        }
+
+        tabs.Add(tabButton);
+    }
+
+    public void OnTabSelected()
+    {
+
+    }
+}
