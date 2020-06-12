8b2c6ff2d9c01160de50a0461f2d3af3392752fe
eclipse130300 <30523791+eclipse130300@users.noreply.github.com>
2020-06-12 17:30:13 +0300

    room_editor_finished?todo sounds+ entrypoint shining

---
+++ Assets/_SuccessProject/Code/Scripts/NEWCODE/Scripts/RoomEditorUIController.cs
@@ -0,0 +1,203 @@
+﻿using CMS.Config;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomEditorUIController : MonoBehaviour
+{
+    [SerializeField] MapConfig playerRoom;
+    [SerializeField] GameObject rightPanel;
+    [SerializeField] Transform parentForItem;
+    [SerializeField] Transform parentForVariants;
+    [SerializeField] GameObject variantPrefab;
+    [SerializeField] GameObject buyButton;
+    [SerializeField] GameObject itemBoughtTab;
+    [SerializeField] GameObject doneButton;
+    [SerializeField] GameObject variantSlider;
+    [SerializeField] TextMeshProUGUI variantCostText;
+    [SerializeField] Image variantCurrencyIMG;
+    [SerializeField] TextMeshProUGUI variantNameIDtext;
+    [SerializeField] TextMeshProUGUI varDescriptionText;
+
+    [SerializeField] Sprite softCurrencySprite;
+    [SerializeField] Sprite hardCurrencySprite;
+
+    public RoomConfig currentRoomConfig;
+    private GameObject itemDisplaying;
+    public RoomPreviewManager previewManager;
+    private ShopManager shopManager;
+    private RoomItemConfig itemCFG;
+
+    [SerializeField] private Vector2 itemDisplaySize = new Vector2(81, 84);
+
+    private void Awake()
+    {
+        shopManager = ShopManager.Instance;
+        previewManager = FindObjectOfType<RoomPreviewManager>();
+        rightPanel.SetActive(false);
+        Messenger.AddListener<GameObject>(GameEvents.ITEM_PRESSED, DisplayItem);
+        Messenger.AddListener(GameEvents.ITEM_OPERATION_DONE, HideItemInfo);
+        Messenger.AddListener<RoomItemConfig, ItemVariant>(GameEvents.ROOM_ITEM_BOUGHT, HideBuyButton);
+
+        Messenger.AddListener<ItemVariant>(GameEvents.ITEM_VARIANT_CHANGED, ManipulateDisplayingInfo);
+    }
+
+    private void Start()
+    {
+        currentRoomConfig = SaveManager.Instance.LoadRoomSet();
+    }
+
+    private void ManipulateDisplayingInfo(ItemVariant var)
+    {
+
+        if (shopManager.CheckIfItemIsBought(itemCFG, var))
+        {
+            buyButton.SetActive(false);
+            itemBoughtTab.SetActive(true);
+            // donebutton on
+            doneButton.SetActive(true);
+            Debug.Log("SHOW DONE BUTTON!!!!");
+
+        }
+        else 
+        {
+            buyButton.SetActive(true);
+            itemBoughtTab.SetActive(false);
+            //donebutton off
+            doneButton.SetActive(false);
+        }
+        if (SaveManager.Instance.LoadRoomSet().ItemAndVarIsInConfig(itemCFG, var))
+        {
+            doneButton.SetActive(false);
+        }
+
+        //if var is active var
+        /*        if(currentRoomConfig.GetActiveVariant(itemCFG) == var)
+                {
+                    doneButton.SetActive(false);
+                }*/
+
+        variantCostText.text = var.cost.ToString();
+
+        variantCurrencyIMG.sprite = var.currencyType == CurrencyType.HARD ? hardCurrencySprite
+            : softCurrencySprite;
+
+        variantNameIDtext.text = var.ConfigId;
+        varDescriptionText.text = var.description;
+    }
+
+
+
+    private void HideBuyButton(RoomItemConfig cfg, ItemVariant var)  //TODO cfg is unnecessary && var
+    {
+        //MAY BE DO NOT HIDE?
+        buyButton.SetActive(false);
+        itemBoughtTab.SetActive(true);
+    }
+
+    public void OnItemBuyTap()
+    {
+        previewManager.TryBuyPreviewingItem();
+    }
+
+    private void HideItemInfo()
+    {
+/*        Destroy(itemDisplaying);*/
+        rightPanel.SetActive(false);
+    }
+
+    private void DisplayItem(GameObject itemGO)
+    {
+        if (currentRoomConfig == null) currentRoomConfig = new RoomConfig();
+        variantSlider.SetActive(true); //activate it just in case
+
+
+        //display item description
+        itemCFG = itemGO.GetComponent<RoomItemDisplay>().itemConfig;
+        rightPanel.SetActive(true);
+        itemDisplaying = Instantiate(itemGO, rightPanel.transform.position, Quaternion.identity);
+        itemDisplaying.transform.SetParent(parentForItem);
+        itemDisplaying.GetComponent<RectTransform>().ResetTransform();
+        itemDisplaying.GetComponent<RectTransform>().sizeDelta = itemDisplaySize;
+        if (shopManager.CheckIfItemIsBought(itemCFG, currentRoomConfig.GetActiveVariant(itemCFG)))
+        {
+            buyButton.SetActive(false);
+        }
+        else
+        {
+            buyButton.SetActive(true);
+        }
+
+
+        //clear all variants
+        var children = parentForVariants.GetComponentsInChildren<Transform>();
+        foreach (Transform child in children)
+        {
+            if (child != parentForVariants.transform) Destroy(child.gameObject);
+        }
+
+
+        // spawn variants in RightSlider
+        if (itemCFG.variants.Count > 1)
+        {
+            foreach (ItemVariant V in itemCFG.variants)
+            {
+                var varGroup = parentForVariants.GetComponent<VariantGroup>();
+                var variant = Instantiate(variantPrefab);
+                variant.transform.SetParent(parentForVariants);
+                var varTab = variant.GetComponent<VariantTab>();
+                varTab.group = varGroup;
+                varTab.variant = V;
+                varTab.group.Subscribe(varTab);
+                varTab.tabBackground.color = V.color;
+                bool hasActiveVar;
+                if (currentRoomConfig.ItemIsInConfig(itemCFG))
+                {
+                    hasActiveVar = currentRoomConfig.GetActiveVariant(itemCFG) == V ? true : false;
+                    Debug.Log("Item: " + itemCFG + ", active variant :" + currentRoomConfig.GetActiveVariant(itemCFG));
+                }
+                else
+                {
+                    hasActiveVar = V == itemCFG.variants[0] ? true : false;
+                }
+                varTab.activeIMG.gameObject.SetActive(hasActiveVar);
+
+                bool isBought = shopManager.CheckIfItemIsBought(itemCFG, V) == true ? false : true;
+                varTab.lockIMG.gameObject.SetActive(isBought);
+            }
+            ManipulateDisplayingInfo(currentRoomConfig.GetActiveVariant(itemCFG));
+        }
+        else
+        {
+            variantSlider.SetActive(false);
+            ManipulateDisplayingInfo(itemCFG.variants[0]);
+        }
+
+        Destroy(itemDisplaying.GetComponent<ItemClick>());
+    }
+
+    public void OnDoneButtonTap()
+    {
+        // add item and variant to config
+        previewManager.OnItemPicked();
+        //hide right panel?
+    }
+
+    public void OnBackButtonTap()
+    {
+        Loader.Instance.LoadGameScene(playerRoom);
+    }
+
+    private void OnDestroy()
+    {
+        Messenger.RemoveListener<GameObject>(GameEvents.ITEM_PRESSED, DisplayItem);
+        Messenger.RemoveListener(GameEvents.ITEM_OPERATION_DONE, HideItemInfo);
+        Messenger.RemoveListener<RoomItemConfig, ItemVariant>(GameEvents.ROOM_ITEM_BOUGHT, HideBuyButton);
+
+        Messenger.RemoveListener<ItemVariant>(GameEvents.ITEM_VARIANT_CHANGED, ManipulateDisplayingInfo);
+    }
+
+
+}
---
+++ Assets/_SuccessProject/Code/Scripts/NEWCODE/Scripts/RoomPreviewManager.cs
@@ -0,0 +1,184 @@
+﻿using CMS.Config;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomPreviewManager : MonoBehaviour
+{
+    public List<GameObject> roomItems;
+
+    public List<GameObject> ObjectsPreviewing;
+
+    public RoomConfig currentRoomConf;
+
+    public FURNITURE furniturePreviewing;
+
+    public SaveManager saveManager;
+
+    public ShopManager shopManager;
+
+    public ItemVariant activeVariant;
+
+    public RoomItemConfig itemPreviewing;
+
+    public Camera previewCamera;
+
+    private RoomCameraMover camMov;
+
+    private void Awake()
+    {
+        saveManager = SaveManager.Instance;
+        shopManager = ShopManager.Instance;
+        camMov = previewCamera.GetComponent<RoomCameraMover>();
+
+        Messenger.AddListener<FURNITURE>(GameEvents.FURNITURE_CHANGED, OnFurnitureChanged);
+        Messenger.AddListener<GameObject>(GameEvents.ITEM_PRESSED, OnItemPressed);
+/*        Messenger.AddListener<RoomItemDisplay>(GameEvents.ROOM_ITEM_PICKED, OnItemPicked);*/
+        Messenger.AddListener<ItemVariant>(GameEvents.ITEM_VARIANT_CHANGED, OnItemVariantChanged);
+        
+    }
+
+    void OnFurnitureChanged(FURNITURE funit)
+    {
+        foreach (GameObject it in roomItems)
+        {
+            if (it.name == funit.ToString())
+            {
+                camMov.target = it.transform;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    void LoadRoomConfig()
+    {
+        currentRoomConf = saveManager.LoadRoomSet();
+        if (currentRoomConf == null)
+            currentRoomConf = new RoomConfig();
+    }
+    void SaveRoomConfig()
+    {
+        saveManager.SaveRoomSet(currentRoomConf);
+    }
+
+    void Initialize()
+    {
+        foreach (string name in Enum.GetNames(typeof(FURNITURE)))
+        {
+            var privewingItems = FindObjectsOfType<IPreviewable>(); //just tag previewing gameobjects with this
+            foreach (var item in privewingItems)
+            {
+                if (item.gameObject.name == name) roomItems.Add(item.gameObject);
+            }
+        }
+
+        LoadRoomConfig();
+    }
+
+    private void OnItemVariantChanged(ItemVariant variant)
+    {
+        foreach (var Gobject in ObjectsPreviewing)
+        {
+            //HERE WE APPLY VARIANT ON THE MODEL!
+            Gobject.GetComponent<MeshRenderer>().material.color = variant.color;
+        }
+        activeVariant = variant;
+    }
+
+/*    public void OnItemPicked(RoomItemDisplay itemDisplay)
+    {
+        if (shopManager.CheckIfItemIsBought(itemPreviewing, activeVariant))
+        {
+            //add item and active variant to config
+            currentRoomConf.AddItemToConfig(itemPreviewing, activeVariant);
+
+            //and save it
+            SaveRoomConfig();
+
+*//*            Messenger.Broadcast(GameEvents.CLOTHES_CHANGED);*//*
+        }
+        else
+        {
+            // make a window - BUY ITEM, BRO
+            Debug.Log("BUY THIS ITEM FIRST!");
+        }
+    }*/
+
+    public void OnItemPicked()
+    {
+        if (shopManager.CheckIfItemIsBought(itemPreviewing, activeVariant))
+        {
+            //add item and active variant to config
+            currentRoomConf.AddItemToConfig(itemPreviewing, activeVariant);
+
+            //and save it
+            SaveRoomConfig();
+
+            Messenger.Broadcast(GameEvents.CLOTHES_CHANGED);
+            Messenger.Broadcast(GameEvents.ROOM_ITEM_PICKED, itemPreviewing);
+            Messenger.Broadcast(GameEvents.ITEM_OPERATION_DONE); //todo it's the same logically as Clothes_changed event?
+        }
+        else
+        {
+            // make a window - BUY ITEM, BRO
+            Debug.Log("BUY THIS ITEM FIRST!");
+        }
+    }
+
+    //show item at model(preview)
+    private void OnItemPressed(GameObject item)
+    {
+        ObjectsPreviewing.Clear();
+        var itemCFG = item.GetComponent<RoomItemDisplay>().itemConfig;
+        activeVariant = currentRoomConf?.GetActiveVariant(itemCFG);
+
+        foreach(GameObject it in roomItems)
+        {
+            if (it.name == itemCFG.furnitureType.ToString())
+            {
+                camMov.target = it.transform;
+                ObjectsPreviewing.Add(it);
+                if (itemCFG.mesh != null)
+                {
+                    it.GetComponent<MeshFilter>().mesh = itemCFG.mesh;
+                }
+
+                var itemRenderer = it.GetComponent<MeshRenderer>();
+                itemRenderer.material = itemCFG.material;
+                itemRenderer.material.color = currentRoomConf?.ItemIsInConfig(itemCFG) == true ?
+        currentRoomConf.GetActiveVariant(itemCFG).color : itemCFG.variants[0].color;
+            }
+        }
+        itemPreviewing = itemCFG;
+    }
+
+    public void TryBuyPreviewingItem()
+    {
+        if (shopManager.CheckIsEnoughMoney(activeVariant.currencyType, activeVariant.cost))
+        {
+            shopManager.Buy(itemPreviewing, activeVariant, activeVariant.cost, activeVariant.currencyType);
+            OnItemPicked();
+            Messenger.Broadcast(GameEvents.ROOM_ITEM_BOUGHT, itemPreviewing, activeVariant);
+        }
+        else
+        {
+            Debug.Log("nope...ADD MONEY FIRST!");
+        }
+    }
+
+
+
+    private void OnDestroy()
+    {
+        Messenger.RemoveListener<FURNITURE>(GameEvents.FURNITURE_CHANGED, OnFurnitureChanged);
+        Messenger.RemoveListener<GameObject>(GameEvents.ITEM_PRESSED, OnItemPressed);
+/*        Messenger.RemoveListener<RoomItemDisplay>(GameEvents.ROOM_ITEM_PICKED, OnItemPicked);*/
+        Messenger.RemoveListener<ItemVariant>(GameEvents.ITEM_VARIANT_CHANGED, OnItemVariantChanged);
+    }
+}
